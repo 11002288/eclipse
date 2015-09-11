80ec680ef22daf9bd050836632aa8e7177df634d
11002288 <Guest@wts6.attlocal.net>
2015-09-10 20:02:29 -0700

    Ethan
    ysgh

---
+++ ethancho/src/ethancho/loopeloop.java
@@ -0,0 +1,47 @@
+package ethancho;
+
+public class loopeloop {
+public static void main(String[] args) {
+	
+//for (int i = 1; i < 501; i++) {
+		
+//if(i%2==0){
+	//System.out.println(i+" even");
+//}else {
+//	System.out.println(i+" odds");
+//}}
+
+
+for (int i = 0; i < 778; i+=7) {
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
